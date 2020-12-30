b90733269ff19a7bc2581a7993c31bff5d89918c
medalotte <medalotte@gmail.com>
2020-12-31 01:40:56 +0900

    refactor inference code

--- files/Ultra96/FPN/fpn_eval/src/main.cc
+++ files/Ultra96/FPN/fpn_eval/src/main.cc
@@ -243,20 +243,23 @@ namespace {
 
     void do_preprocess() {
         cv::Mat resized_img(dpu_inout_info.in_size, CV_8UC3);
-        for (size_t im_i = 0; im_i < read_buffer.size(); im_i++) {
+        auto img_src = read_buffer.begin();
+        auto end_flag = false;
+        while(!end_flag) {
             /***** PREPROCESS FOR INFERENCE *****/
 #if DEBUG_MODE
             const auto t0 = std::chrono::system_clock::now();
 #endif
 
-            cv::resize(read_buffer[im_i], resized_img, resized_img.size(), 0, 0, cv::INTER_LINEAR);
+            cv::resize(*img_src, resized_img, resized_img.size(), 0, 0, cv::INTER_LINEAR);
+            img_src++;
 
 #if DEBUG_MODE
             const auto t1 = std::chrono::system_clock::now();
 #endif
 
-            const auto is_last = (bool)(im_i == read_buffer.size() - 1);
-            preproc_fifo.write(no_abnormality, is_last, [&](PreprocFIFOElementType& dst) -> void {
+            end_flag = static_cast<bool>(img_src == read_buffer.end());
+            preproc_fifo.write(no_abnormality, end_flag, [&](PreprocFIFOElementType& dst) -> void {
                 dpuProcessNormalizion(dst.data(), resized_img.data, resized_img.rows, resized_img.cols, dpu_inout_info.in_mean,
                                       dpu_inout_info.in_scale_fix, resized_img.step1());
             });
@@ -328,20 +331,20 @@ namespace {
     void do_postprocess() {
         cv::Mat seg(dpu_inout_info.out_size, CV_8UC1);
 
-        auto im_i = 0U;
-        auto end_flag = false;
-        while (!end_flag) {
+        auto img_dst = write_buffer.begin();
+        while (img_dst != write_buffer.end()) {
             /***** POSTPROCESS OF INFERENCE *****/
 #if DEBUG_MODE
             const auto t0 = std::chrono::system_clock::now();
 #endif
 
             postproc_fifo.read(no_abnormality, [&](const PostprocFIFOElementType& dst) -> void {
+                auto offset = dst.data();
                 for (int ri = 0; ri < seg.rows; ri++) {
                     for (int ci = 0; ci < seg.cols; ci++) {
-                        const auto idx = (ri * seg.cols + ci) * DPU_OUTPUT_NOF_CLASS;
-                        const auto max_idx = std::max_element(dst.data() + idx, dst.data() + idx + DPU_OUTPUT_NOF_CLASS);
-                        seg.at<unsigned char>(ri, ci) = (unsigned char)(std::distance(dst.data() + idx, max_idx));
+                        const auto max_idx = std::max_element(offset, offset + DPU_OUTPUT_NOF_CLASS);
+                        seg.at<uint8_t>(ri, ci) = static_cast<uint8_t>(std::distance(offset, max_idx));
+                        offset += DPU_OUTPUT_NOF_CLASS;
                     }
                 }
             });
@@ -350,7 +353,8 @@ namespace {
             const auto t1 = std::chrono::system_clock::now();
 #endif
 
-            cv::resize(seg, write_buffer[im_i], write_buffer[im_i].size(), 0, 0, cv::INTER_NEAREST);
+            cv::resize(seg, *img_dst, (*img_dst).size(), 0, 0, cv::INTER_NEAREST);
+            img_dst++;
 
 #if DEBUG_MODE
             const auto t2 = std::chrono::system_clock::now();
@@ -365,9 +369,6 @@ namespace {
             }
 #endif
             /***** END *****/
-
-            end_flag = postproc_fifo.neverReadNextElement();
-            im_i++;
         }
     }
 
