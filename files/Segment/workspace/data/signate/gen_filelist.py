0ebf8ff5d0c705f8a8fb1b85b4a594c4c6edd17e
lp6m <lp6m2014@gmail.com>
2021-01-03 02:35:41 +0000

    remove files/Segment/workspace/data from git

--- files/Segment/workspace/data/signate/gen_filelist.py
+++
@@ -1,8 +0,0 @@
-import os
-import glob
-
-files = glob.glob("./seg_train_images/*.jpg")
-files = sorted(list(files))
-for file in files:
-    basename = os.path.basename(file).replace('.jpg', '')
-    print(basename)
