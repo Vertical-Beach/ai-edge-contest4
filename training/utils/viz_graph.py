0734440c1630892f674ed4516677c24266758c70
lp6m <lp6m2014@gmail.com>
2021-01-03 02:24:02 +0000

    add main to viz_graph.py

--- training/utils/viz_graph.py
+++ training/utils/viz_graph.py
@@ -16,3 +16,14 @@ def draw_loss_curve(t_loss, v_loss, path):
 	plt.ylim(0.17, 0.40)
 	plt.savefig(path)
 	plt.clf()
+
+if __name__ == "__main__":
+	model_name = sys.argv[1]
+	v_loss = None
+	t_loss = None
+	with open(model_name + "/epoch_losses_val.pkl", "rb") as file:
+		v_loss = pickle.load(file)
+
+	with open(model_name + "/epoch_losses_train.pkl", "rb") as file:
+		t_loss = pickle.load(file)
+	print(np.argmin(v_loss), np.min(v_loss))
